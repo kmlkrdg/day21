b8c9a919061e39400634fd94bf7b60fdb5f54da0
kmlkrdg <kemalkaradag52@gmail.com>
2023-03-06 13:43:55 +0300

    day21

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './pages/product-list/product-list.component';
+import { ProductCreateComponent } from './pages/product-create/product-create.component';
+import { ProductUpdateComponent } from './pages/product-update/product-update.component';
+import { ProductDeleteComponent } from './pages/product-delete/product-delete.component';
+import { ProductListWithOkComponent } from './pages/product-list-with-ok/product-list-with-ok.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent,
+    ProductCreateComponent,
+    ProductUpdateComponent,
+    ProductDeleteComponent,
+    ProductListWithOkComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+  
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/states/product-state.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+import { Product } from '../models/product';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductStateService {
+
+  public productList:Product[]=[];
+  constructor() { }
+}
